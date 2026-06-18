7f663526342e5388fd185056e5b6e404731a135f
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-06-18 12:36:53 -0600

    feat: implement fade in and fade out animations with themed buttons

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,22 +1,60 @@
+import { useRef } from 'react'
+import { Animated, Easing } from 'react-native'
+
 import ThemedButton from '@/presentation/shared/ThemedButton'
 import ThemedView from '@/presentation/shared/ThemedView'
-import { console } from 'react-native'
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current
+  const animatedTop = useRef(new Animated.Value(-100)).current
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start()
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      easing: Easing.bounce
+    }).start()
+  }
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true
+    }).start(() => animatedTop.resetAnimation())
+  }
+
   return (
-    // 1. Usamos nuestro contenedor con margen
-    <ThemedView margin>
-      {/* 2. Botón 1 con margen inferior (mb-5) */}
-      <ThemedButton className="mb-5" onPress={() => console.log('fadeIN')}>
+    <ThemedView margin className="justify-center items-center flex-1">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+          transform: [
+            {
+              translateY: animatedTop
+            }
+          ]
+        }}
+      />
+
+      <ThemedButton className="my-5" onPress={fadeIn}>
         Fade In
       </ThemedButton>
 
-      {/* 3. Botón 2 */}
-      <ThemedButton className="mb-5" onPress={() => console.log('fadeOUT')}>
+      <ThemedButton className="my-5" onPress={fadeOut}>
         FadeOut
       </ThemedButton>
     </ThemedView>
   )
 }
-
 export default Animation101Screen
